39e63fd7848c926dff6e7735e6c92148c7e63079
madforlife <madforlifee@gmail.com>
2024-10-16 12:01:19 +0300

    Configure spring-security

---
+++ src/main/java/net/littlemad/socialapp/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package net.littlemad.socialapp.config;
+
+import net.littlemad.socialapp.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/signup").permitAll()
+                        .requestMatchers("/v1/roles/**").hasAuthority("ADMIN")
+                        .requestMatchers("/v1/users/**").hasAuthority("ADMIN")
+                        .requestMatchers("/v1/admins").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+}
